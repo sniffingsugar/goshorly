1588acb526f1dba2542df7b06905bb2756559c6a
Phil <phil@ucode.space>
2021-12-08 16:04:52 +0100

    stash

--- db/connection.go
+++ db/connection.go
@@ -3,12 +3,11 @@ package db
 import (
 	"log"
 
-	"git.ucode.space/Phil/goshorly/utils"
 	"github.com/go-redis/redis"
 )
 
 var Client *redis.Client = redis.NewClient(&redis.Options{
-	Addr:     utils.REDIS_URI + ":6379",
+	Addr:     "redis:6379",
 	Password: "",
 	DB:       0,
 })
